d465b607c6976ccee9df12b0820612f15481a59b
Amit Batra <amitbatra3101@gmail.com>
2021-10-17 20:31:39 +0530

    Add items

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const Date = require(__dirname + "/date.js");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 const port = 5000;
 app.set("view engine", "ejs");
-const listItems = ["Read Book", "Learn EJS", "Make Project"];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+const itemsSchema = new mongoose.Schema({
+  itemName: String,
+});
+const Item = mongoose.model("Item", itemsSchema);
+
+// const listItems = ["Read Book", "Learn EJS", "Make Project"];
+// const workItems = [];
+const task1 = new Item({
+  itemName: "Read Book",
+});
+const task2 = new Item({
+  itemName: "Learn EJS",
+});
+const task3 = new Item({
+  itemName: "Make Project",
+});
+const defaultArray = [task1, task2, task3];
+Item.insertMany(defaultArray, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Inserted Items successfully");
+  }
+});
 app.get("/", (req, res) => {
   const Day = Date.getDate();
   res.render("list", { listTitle: Day, Items: listItems });
